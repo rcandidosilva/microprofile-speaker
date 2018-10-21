8847497c1d73af551b76c07c1ab0867cf74d1def
Rodrigo Cândido da Silva <rcandidosilva@gmail.com>
2018-10-21 14:41:01 -0700

    Changes at kubernetes.yml

--- src/main/java/com/microprofile/speaker/rest/SpeakerApplication.java
+++ src/main/java/com/microprofile/speaker/rest/SpeakerApplication.java
@@ -1,8 +1,12 @@
 package com.microprofile.speaker.rest;
 
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("/")
+@OpenAPIDefinition(info = @Info(title = "Speaker", version = "1.0.0"))
 public class SpeakerApplication extends Application {
 }
--- src/main/java/com/microprofile/speaker/rest/SpeakerResource.java
+++ src/main/java/com/microprofile/speaker/rest/SpeakerResource.java
@@ -1,6 +1,10 @@
 package com.microprofile.speaker.rest;
 
 import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
@@ -19,6 +23,10 @@ public class SpeakerResource {
     @GET
     @Produces("application/json")
     @Counted(name = "getAll", absolute = true, monotonic = true, description = "Number the times requested")
+    @Operation(summary = "Get all speakers")
+    @APIResponse(responseCode = "200",
+            description = "List of speakers",
+            content = @Content(schema = @Schema(implementation = Speaker.class)))
     public Response getAll() {
         return Response.ok(service.getSpeakers()).build();
     }
@@ -26,7 +34,10 @@ public class SpeakerResource {
     @GET
     @Path("/{id}")
     @Produces("application/json")
-    @Counted(name = "getAll", absolute = true, monotonic = true, description = "Number the times requested")
+    @Operation(summary = "Get a speaker by id")
+    @APIResponse(responseCode = "200",
+            description = "Speaker data",
+            content = @Content(schema = @Schema(implementation = Speaker.class)))
     public Response getById(@PathParam("id") Integer id) {
         Optional<Speaker> speaker = service.getById(id);
         if (speaker.isPresent()) {
